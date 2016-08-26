6d7ccdc0d53c7a08458346dc0cb6faf945bea76e
Mark Karavan <karavan@Marks-MacBook-Pro.local>
2016-08-26 12:45:08 -0400

    adds sublime

--- bash_commands.sh
+++ bash_commands.sh
@@ -3,6 +3,11 @@ alias brc='vim ~/bash_shortcuts/bash_commands.sh'
 alias brcls='cd ~/bash_shortcuts/ && ll'
 alias .brc='source ~/.bash_profile'
 
+# Sublime shortcut
+alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
+# Make sublime default editor
+export EDITOR='subl -w'
+
 export PATH=$PATH:/usr/local/bin
 # from laravel installation
 export PATH="~/.composer/vendor/bin:$PATH"
