18d29f22b162e752ebee220a9df7a5b197e48f22
Mark Karavan <mark@finsight.com>
2016-09-30 11:36:48 -0400

    adds some docker shortcuts

--- docker_shortcuts.sh
+++ docker_shortcuts.sh
@@ -1,3 +1,8 @@
 alias edmed='eval "$(docker-machine env default)"'
 alias dm="docker-machine"
 alias dc="docker-compose"
+alias di="docker images"
+alias dps="docker ps -a"
+alias db="docker build"
+alias dr="docker run"
+
