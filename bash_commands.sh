7ef7492c9126d45c3ea4a8d30bfdc3f0bbb63e8a
Mark Karavan <mark@finsight.com>
2017-01-27 16:21:46 -0500

    adds inifinite history

--- bash_commands.sh
+++ bash_commands.sh
@@ -18,3 +18,19 @@ export PATH="$(brew --prefix homebrew/php/php56)/bin:$PATH"
 # mysql
 export PATH=${PATH}:/usr/local/mysql/bin/
 
+# composer.phar
+alias composer="php /usr/local/bin/composer.phar"
+
+# Eternal bash history.
+# ---------------------
+# Undocumented feature which sets the size to "unlimited".
+# http://stackoverflow.com/questions/9457233/unlimited-bash-history
+export HISTFILESIZE=
+export HISTSIZE=
+export HISTTIMEFORMAT="[%F %T] "
+# Change the file location because certain bash sessions truncate .bash_history file upon close.
+# http://superuser.com/questions/575479/bash-history-truncated-to-500-lines-on-each-login
+export HISTFILE=~/.bash_eternal_history
+# Force prompt to write history after every command.
+# http://superuser.com/questions/20900/bash-history-loss
+PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
